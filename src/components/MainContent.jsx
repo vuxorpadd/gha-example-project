57464c042e594e97487aa80b9a8e1e9f6c6d3ea0
pavlo.zaporozkyi <pavlo.zaporozkyi@mev.com>
2023-06-14 21:11:37 +0300

    Remove button

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -11,7 +11,6 @@ function MainContent() {
 
   return (
     <main>
-      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
       {helpVisible && <HelpArea />}
     </main>
   );
